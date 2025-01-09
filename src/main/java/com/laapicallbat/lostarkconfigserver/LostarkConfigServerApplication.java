bc874109610d02da17f74f9de21bbf07134bef30
gimjinho <poiuy9954@gamil.com>
2025-01-09 12:07:52 +0900

    LostarkConfigServer 생성

---
+++ src/main/java/com/laapicallbat/lostarkconfigserver/LostarkConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.laapicallbat.lostarkconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class LostarkConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostarkConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laapicallbat/lostarkconfigserver/LostarkConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laapicallbat.lostarkconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostarkConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
