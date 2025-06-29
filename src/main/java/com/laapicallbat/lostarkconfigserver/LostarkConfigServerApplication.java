cdae6a176e02d5eb6f6dbb7f39bdfbb663d6b17e
gimjinho <poiuy9954@gamil.com>
2025-06-29 19:16:24 +0900

    batch작업 개발

--- src/main/java/com/laapicallbat/lostarkconfigserver/LostarkConfigServerApplication.java
+++ src/main/java/com/laapicallbat/lostarkconfigserver/LostarkConfigServerApplication.java
@@ -1,15 +1,19 @@
 package com.laapicallbat.lostarkconfigserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@Slf4j
 @SpringBootApplication
 @EnableConfigServer
 public class LostarkConfigServerApplication {
 
     public static void main(String[] args) {
+        log.info("=== Lostark Config Server Starting ===");
         SpringApplication.run(LostarkConfigServerApplication.class, args);
+        log.info("=== Lostark Config Server Started Successfully ===");
     }
 
 }
